3758187acde20f75c677783358419a21bd5cad9b
Yazor <89312042+Yaazor@users.noreply.github.com>
2025-08-15 12:05:05 -0400

    Création de components, refactoring css

---
+++ src/scripts/index.ts
@@ -0,0 +1,8 @@
+export enum WebsiteColor {
+    PRIMARY = "--color-primary",
+    SECONDARY = "--color-secondary",
+    ACCENT = "--color-accent",
+
+    PAGE = "--color-page",
+    CONTRAST = "--color-contrast"
+}
