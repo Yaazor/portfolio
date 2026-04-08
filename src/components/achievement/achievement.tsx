db6959d0d4ee0dbb5bc0deed3cd81741cb05317b
Yazor <yazor@herobrine.fr>
2026-04-08 17:18:40 -0400

    attempt to fix tailwind

--- src/components/achievement/achievement.tsx
+++ src/components/achievement/achievement.tsx
@@ -5,7 +5,7 @@ import { TypographyH2 } from "../ui/Typography";
 
 export default function(props: React.PropsWithChildren) {
     return (
-        <CarouselItem className="basis-1/3 items-stretch">
+        <CarouselItem className="items-stretch md:basis-1/3">
             <Card>
                 <CardHeader className="flex flex-2 text-center flex-col">
                     {props.children}
